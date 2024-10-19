2084d0e16bb72eba5209677159b03aa23b92eb7e
SusLiu03 <susannl5@uci.edu>
2024-10-19 03:41:40 -0700

    updated home page ui

--- client/src/features/home/HomePage.jsx
+++ client/src/features/home/HomePage.jsx
@@ -1,25 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./home.css";
 import TopBar from "../../components/topbar";
 import SideBar from "../../components/sidebar_mobile";
-
+import StreakCard from "../../components/streak_card";
+import ApplicationList from "../../components/application_list";
 function HomePage() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false); // State to manage sidebar visibility
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen); // Toggle the sidebar state
   };
+  useEffect(() => {
+    const handleResize = () => {
+      // Close the sidebar if the screen width is greater than or equal to 640px (sm breakpoint)
+      if (window.innerWidth >= 640) {
+        setIsSidebarOpen(false);
+      }
+    };
+
+    window.addEventListener("resize", handleResize); // Add the resize event listener
+    return () => {
+      window.removeEventListener("resize", handleResize); // Clean up the event listener on unmount
+    };
+  }, []);
 
   return (
-    <div className="home-container m-0 p-0 relative h-screen">
-      <div className="relative">
-        <TopBar />
-        <SideBar
-          isOpen={isSidebarOpen}
-          className="absolute visibility:hidden"
-          toggleSidebar={toggleSidebar}
-        />
-      </div>
+    <div className="home-container m-0 p-0 relative h-full">
+      <TopBar />
+      <SideBar
+        isOpen={isSidebarOpen}
+        className="absolute visibility:hidden"
+        toggleSidebar={toggleSidebar}
+      />
       <div className="flex items-center justify-between top-4 left-0 right-0 z-10 sm:hidden">
         <button onClick={toggleSidebar} className="m-4 p-2 rounded">
           <img
@@ -32,17 +44,21 @@ function HomePage() {
             className="w-6 h-6" // Adjust size as necessary
           />
         </button>
-        <button
-          className="m-4 p-4 px-6 text-2xl text-gray-300 rounded-3xl"
-          style={{ backgroundColor: "#495195" }}
-        >
-          Profile
+        <button className="m-4 p-1 rounded-full border-4 border-[#495195] bg-transparent flex items-center justify-center">
+          <img
+            src="/icons8-user-100.png" // Ensure the path is correct
+            alt="Profile"
+            className="w-8 h-8" // Adjust size as necessary
+          />
         </button>
       </div>
       <div className="hidden sm:flex items-center justify-between top-0 left-0 right-0 z-10 p-4">
         <div className="flex space-x-8 pl-16">
           <button className="bg-transparent text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
             Home
+          </button>
+          <button className="bg-transparent text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
+            About Us
           </button>
           <button className="bg-transparent text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
             Jobs
@@ -57,21 +73,24 @@ function HomePage() {
             Certifications
           </button>
         </div>{" "}
-        <button
-          className="m-4 p-4 px-6 text-2xl text-gray-300 rounded-3xl"
-          style={{ backgroundColor: "#495195" }}
-        >
-          Profile
+        <button className="m-4 p-2 rounded-full border-4 border-[#495195] bg-transparent flex items-center justify-center">
+          <img
+            src="/icons8-user-100.png" // Ensure the path is correct
+            alt="Profile"
+            className="w-10 h-10" // Adjust size as necessary
+          />
         </button>
       </div>
-      <div className="content p-4">
+      <div className="content py-4">
         <h2
-          className="text-2xl text-black font-bold p-10"
+          className="text-2xl lg:text-4xl text-black font-bold p-10 lg:p-20"
           style={{ backgroundColor: "#F6C344" }}
         >
-          Welcome Priyanka Koppula
+          Overview
         </h2>
       </div>
+      <StreakCard />
+      <ApplicationList />
     </div>
   );
 }
--- client/src/features/login/LoginPage.jsx
+++ client/src/features/login/LoginPage.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import './login.css'
-import { login } from '../../lib/auth';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./login.css";
+import { login } from "../../lib/auth";
+import { Link } from "react-router-dom";
 function LoginPage() {
   // States for handling user input
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   // Handle form submission
   const handleSubmit = (e) => {
@@ -13,10 +13,10 @@ function LoginPage() {
     console.log("Username:", username);
     console.log("Password:", password);
     // You would send the data to the backend for authentication here
-    
+
     login(username, password);
   };
-  
+
   return (
     <header className="App-header">
       <h1>ReBirth Empowerment Education</h1>
@@ -24,19 +24,19 @@ function LoginPage() {
         Empowering people to find jobs and equip themselves for success
       </p>
       <form className="sign-in-form" onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          placeholder="Enter your username" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)} 
-          required 
+        <input
+          type="text"
+          placeholder="Enter your username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
         />
-        <input 
-          type="password" 
-          placeholder="Enter your password" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
-          required 
+        <input
+          type="password"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
         />
         <button type="submit">Sign In</button>
       </form>
@@ -46,9 +46,15 @@ function LoginPage() {
 
       <div className="additional-signin">
         <h3>Are you a Mentor, Employer, or Administrator?</h3>
-        <a href="/mentor-signin">Mentor or Alumni? <strong>Sign in here</strong></a>
-        <a href="/employer-signin">Employer looking for talent? <strong>Sign in here</strong></a>
-        <a href="/admin-signin">Administrator? <strong>Sign in here</strong></a>
+        <a href="/mentor-signin">
+          Mentor or Alumni? <strong>Sign in here</strong>
+        </a>
+        <a href="/employer-signin">
+          Employer looking for talent? <strong>Sign in here</strong>
+        </a>
+        <a href="/admin-signin">
+          Administrator? <strong>Sign in here</strong>
+        </a>
       </div>
     </header>
   );
