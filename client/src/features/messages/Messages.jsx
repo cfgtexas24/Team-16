8c92f9e47be55d123bb75cff159d58c61292d64f
Ohimoiza1205 <omoiza@ttu.edu>
2024-10-19 11:19:01 -0500

    Update Messages sent

--- client/src/features/messages/Messages.jsx
+++ client/src/features/messages/Messages.jsx
@@ -13,10 +13,10 @@ const Messages = () => {
       <ul className="message-list">
         {/* Message 1 */}
         <li className="message-item">
-          <img src="./images/jpm.jpg" alt="Mentor Paul" className="avatar" />
+          <img src="client\public\jpm.jpg" alt="Mentor Paul" className="avatar" />
           <div className="message-content">
             <h3>Mentor Paul</h3>
-            <p>Hey, could you schedule a time for...</p>
+            <p>And to follow-up, could you schedule a time for our bi-weekly mentor meetings</p>
           </div>
           <div className="message-meta">  {/* Wrapper div for time and unread count */}
             <span className="time">5 min</span>
@@ -24,10 +24,10 @@ const Messages = () => {
           </div>
         </li>
         <li className="message-item">
-          <img src="client/src/jpm.jpg" alt="JPM Recruiting" className="avatar" />
+          <img src="client\public\jpm.jpg" alt="JPM Recruiting" className="avatar" />
           <div className="message-content">
             <h3>JPM Recruiting</h3>
-            <p>Thank you for applying to our Soft...</p>
+            <p>Thank you for applying to our Software Engineeering DevOps role at JPMorgan Chase. We're still reviewing your application and would get back...</p>
           </div>
           <span className="time">12 min</span>
         </li>
@@ -35,23 +35,23 @@ const Messages = () => {
           <img src="./images/rebirth_empowerment_logo.jpg" alt="Mentor Priyanka" className="avatar" />
           <div className="message-content">
             <h3>Mentor Priyanka</h3>
-            <p>Hello! My name is Priyanka and I'm...</p>
+            <p>Hello! My name is Priyanka and I'm your assigned mentor at ReBirth Empowerment Education</p>
           </div>
           <span className="time">48 min</span>
         </li>
         <li className="message-item">
-          <img src="C:/Users/ohimo/OneDrive/Documents/SSHKeys/ReBirthFrontPage/rebirth_empowerment_logo.jpg" alt="ReBirth Empowerment" className="avatar" />
+          <img src="client\public\rebirth_empowerment_logo.jpg" alt="ReBirth Empowerment" className="avatar" />
           <div className="message-content">
             <h3>ReBirth Empowerment</h3>
-            <p>Congratulations! You have recently...</p>
+            <p>Congratulations! You have recently activated your application portal at ReBirth Empowerment. Ensure to go through the resources tab to find ou...</p>
           </div>
           <span className="time">1 hour</span>
         </li>
         <li className="message-item">
-          <img src="C:/Users/ohimo/OneDrive/Documents/SSHKeys/ReBirthFrontPage/rebirth_empowerment_logo.jpg" alt="ReBirth Empowerment" className="avatar" />
+          <img src="client\public\rebirth_empowerment_logo.jpg" alt="ReBirth Empowerment" className="avatar" />
           <div className="message-content">
             <h3>ReBirth Empowerment</h3>
-            <p>Your one-time activation code is 0...</p>
+            <p>Your one-time activation code is 085-263</p>
           </div>
           <span className="time">5 hours</span>
         </li>
