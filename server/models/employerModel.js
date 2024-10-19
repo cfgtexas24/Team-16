7c33668e456cff295cdcf05764e95d71b420bde3
lucas <lucasmach@ufl.edu>
2024-10-18 23:34:18 -0500

    add hashing for different models, employees, caseManager, admin

--- server/models/employerModel.js
+++ server/models/employerModel.js
@@ -34,6 +34,35 @@ const employerSchema = new mongoose.Schema({
   timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
 });
 
+// Pre-save hook to hash the password before saving
+employerSchema.pre('save', async function (next) {
+  const user = this;
+
+  // Only hash the password if it's new or has been modified
+  if (!user.isModified('password')) return next();
+
+  try {
+      // Generate a salt
+      const salt = await bcrypt.genSalt(10);
+      
+      // Hash the password using the salt
+      const hash = await bcrypt.hash(user.password, salt);
+      
+      // Replace the plain text password with the hashed one
+      user.password = hash;
+
+      // Proceed to the next middleware or save the document
+      next();
+  } catch (err) {
+      next(err);
+  }
+});
+
+// Method to compare the entered password with the hashed password
+employerSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
 const Employer = mongoose.model('Employer', employerSchema);
 
 module.exports = Employer;
