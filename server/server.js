ea107a138e3fc625bb05682ffb91212aa8affdf2
sir-axel <niy.axel@gmail.com>
2024-10-18 19:26:47 -0500

    add listen before db connect

--- server/server.js
+++ server/server.js
@@ -11,17 +11,17 @@ app.use(cors());
 app.use(express.json());
 
 
-
+// Listen for requests
+app.listen(process.env.PORT, () => {
+    console.log(`Connected to DB and Listening on port ${process.env.PORT}`)
+})
 
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
-        // Listen for requests
-        app.listen(process.env.PORT, () => {
-            console.log(`Connected to DB and Listening on port ${process.env.PORT}`)
-        })
+        console.log("Connected.");
     })
     .catch((error) => {
-        console.log(error)
+        console.error(error)
     })
