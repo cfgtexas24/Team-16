f026261bb5c4409405231998160732392c64dad2
SusLiu03 <susannl5@uci.edu>
2024-10-19 09:20:38 -0700

    fixed issues in sidebar

--- client/src/components/sidebar_mobile.jsx
+++ client/src/components/sidebar_mobile.jsx
@@ -9,44 +9,29 @@ const SideBar = ({ isOpen, toggleSidebar }) => {
     >
       <div className="flex flex-col">
         {/* Flex column for vertical layout */}
-        <a href="/home">
-          <button className="bg-white m-0 text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            Home
-          </button>
-        </a>
-        <a href="/about">
-          <button className="bg-transparent text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            About Us
-          </button>
-        </a>
-        <a href="/jobs">
-          <button className="bg-white m-0 text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            Jobs
-          </button>
-        </a>
-        <a href="/messages">
-          <button className="bg-white m-0 text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            Messages
-          </button>
-        </a>
-        <a href="/training">
-          <button className="bg-white m-0 text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            Training
-          </button>
-        </a>
-        <a href="/certifications">
-          <button className="bg-white m-0 text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            Certifications
-          </button>
-        </a>
-        <a href="/resources">
-          <button className="bg-white m-0 text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            Resources
-          </button>
-        </a>
+        <button className="bg-white m-0 w-full text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
+          Home
+        </button>
+        <button className="bg-transparent text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
+          About Us
+        </button>
+        <button className="bg-white m-0 text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
+          Jobs
+        </button>
+        <button className="bg-white m-0 text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
+          Messages
+        </button>
+        <button className="bg-white m-0 text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
+          Training
+        </button>
+        <button className="bg-white m-0 text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
+          Certifications
+        </button>
+        <button className="bg-white m-0 text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
+          Resources
+        </button>
       </div>
     </div>
   );
 };
-
 export default SideBar;
