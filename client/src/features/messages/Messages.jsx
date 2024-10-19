3c9474a6fcf00b6cedf1ad7952a21451a2b6cb7d
Ohimoiza1205 <omoiza@ttu.edu>
2024-10-19 10:33:04 -0500

    Added bottom icons to messages

--- client/src/features/messages/Messages.jsx
+++ client/src/features/messages/Messages.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './Messages.css';  // Ensure you have the styles in this CSS file
+import { Home, MessageSquare, User } from 'lucide-react'; // Importing icons
+import './Messages.css'; // Ensure you have the styles in this CSS file
 
 const Messages = () => {
   return (
@@ -17,11 +18,11 @@ const Messages = () => {
             <h3>Mentor Paul</h3>
             <p>Hey, could you schedule a time for...</p>
           </div>
-          <span className="time">5 min</span>
-          <span className="unread-count">2</span>
+          <div className="message-meta">  {/* Wrapper div for time and unread count */}
+            <span className="time">5 min</span>
+            <span className="unread-count">2</span> {/* Yellow unread count */}
+          </div>
         </li>
-
-        {/* Message 2 */}
         <li className="message-item">
           <img src="client/src/jpm.jpg" alt="JPM Recruiting" className="avatar" />
           <div className="message-content">
@@ -30,8 +31,6 @@ const Messages = () => {
           </div>
           <span className="time">12 min</span>
         </li>
-
-        {/* Message 3 */}
         <li className="message-item">
           <img src="./images/rebirth_empowerment_logo.jpg" alt="Mentor Priyanka" className="avatar" />
           <div className="message-content">
@@ -40,8 +39,6 @@ const Messages = () => {
           </div>
           <span className="time">48 min</span>
         </li>
-
-        {/* Message 4 */}
         <li className="message-item">
           <img src="C:/Users/ohimo/OneDrive/Documents/SSHKeys/ReBirthFrontPage/rebirth_empowerment_logo.jpg" alt="ReBirth Empowerment" className="avatar" />
           <div className="message-content">
@@ -50,8 +47,6 @@ const Messages = () => {
           </div>
           <span className="time">1 hour</span>
         </li>
-
-        {/* Message 5 */}
         <li className="message-item">
           <img src="C:/Users/ohimo/OneDrive/Documents/SSHKeys/ReBirthFrontPage/rebirth_empowerment_logo.jpg" alt="ReBirth Empowerment" className="avatar" />
           <div className="message-content">
@@ -63,12 +58,19 @@ const Messages = () => {
       </ul>
 
       <footer className="footer">
-        <button className="footer-btn home-btn"></button>
-        <button className="footer-btn message-btn active"></button>
-        <button className="footer-btn settings-btn"></button>
+        <button className="footer-btn home-btn">
+          <Home size={24} /> {/* Icon for Home */}
+        </button>
+        <button className="footer-btn message-btn active">
+          <MessageSquare size={24} /> {/* Icon for Messages */}
+        </button>
+        <button className="footer-btn settings-btn">
+          <User size={24} /> {/* Icon for User/Profile */}
+        </button>
       </footer>
     </div>
   );
 };
+
 
 export default Messages;
