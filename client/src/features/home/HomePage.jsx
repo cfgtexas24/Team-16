a3c22460128085bd546a9132b801f2dbc1291cef
SusLiu03 <susannl5@uci.edu>
2024-10-19 04:48:45 -0700

    Updated sidebar and home page; added Navbar component

--- client/src/features/home/HomePage.jsx
+++ client/src/features/home/HomePage.jsx
@@ -4,6 +4,8 @@ import TopBar from "../../components/topbar";
 import SideBar from "../../components/sidebar_mobile";
 import StreakCard from "../../components/streak_card";
 import ApplicationList from "../../components/application_list";
+import Navbar from "../../components/navbar"; // Import the Navbar component
+
 function HomePage() {
   const [n, setName] = useState("");
   useEffect(() => {
@@ -11,12 +13,13 @@ function HomePage() {
     // const { name } = decodedToken;
     // setName(name);
   }, []);
+
   const [isSidebarOpen, setIsSidebarOpen] = useState(false); // State to manage sidebar visibility
-  // const navigate = useNavigate(); // Initialize useNavigate
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen); // Toggle the sidebar state
   };
+
   useEffect(() => {
     const handleResize = () => {
       // Close the sidebar if the screen width is greater than or equal to 640px (sm breakpoint)
@@ -51,43 +54,17 @@ function HomePage() {
             className="w-6 h-6" // Adjust size as necessary
           />
         </button>
-        <button className="m-4 p-1 rounded-full border-4 border-[#495195] bg-transparent flex items-center justify-center">
-          <img
-            src="/icons8-user-100.png" // Ensure the path is correct
-            alt="Profile"
-            className="w-8 h-8" // Adjust size as necessary
-          />
-        </button>
+        <a href="/profile" className="m-4">
+          <button className="p-2 rounded-full border-4 border-[#495195] bg-transparent flex items-center justify-center">
+            <img
+              src="/icons8-user-100.png" // Ensure the path is correct
+              alt="Profile"
+              className="w-10 h-10" // Adjust size as necessary
+            />
+          </button>
+        </a>
       </div>
-      <div className="hidden sm:flex items-center justify-between top-0 left-0 right-0 z-10 p-4">
-        <div className="flex space-x-8 pl-16">
-          <button className="bg-transparent text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            Home
-          </button>
-          <button className="bg-transparent text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            About Us
-          </button>
-          <button className="bg-transparent text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            Jobs
-          </button>
-          <button className="bg-transparent text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            Messages
-          </button>
-          <button className="bg-transparent text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            Training
-          </button>
-          <button className="bg-transparent text-2xl text-black p-4 rounded hover:bg-[#495195] hover:text-gray-300">
-            Certifications
-          </button>
-        </div>{" "}
-        <button className="m-4 p-2 rounded-full border-4 border-[#495195] bg-transparent flex items-center justify-center">
-          <img
-            src="/icons8-user-100.png" // Ensure the path is correct
-            alt="Profile"
-            className="w-10 h-10" // Adjust size as necessary
-          />
-        </button>
-      </div>
+      <Navbar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <div className="content py-4">
         <h2
           className="text-2xl lg:text-4xl text-black font-bold p-10 lg:p-20"
