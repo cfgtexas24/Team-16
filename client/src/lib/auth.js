5d9bd16a62eca959309817864666212c5eda38d0
Jason Antwi-Appah <hey@jasonaa.me>
2024-10-19 00:03:11 -0500

    feat: add register page, persist token to localstorage

---
+++ client/src/lib/auth.js
@@ -0,0 +1,75 @@
+const LOCAL_STORAGE_JWT_KEY = 'cfg_jwt';
+
+/**
+ * Performs an authenticated fetch request and handles unauthorized access.
+ * @param {string} url - The URL to fetch.
+ * @param {RequestInit} [options] - The options for the fetch request.
+ * @returns {Promise<any>} The parsed JSON response data.
+ * @throws {Error} If the fetch request fails.
+ */
+export const fetchWithAuth = async (url, options) => {
+  const token = localStorage.getItem(LOCAL_STORAGE_JWT_KEY)
+  if (!token) {
+    throw new Error("No token found in local storage when trying to fetch an authenticated route.");
+  }
+  
+  const headers = options?.headers || {};
+  headers["Authorization"] = `Bearer ${token}`;
+  options.headers = headers;
+  
+  const response = await fetch(process.env.REACT_APP_BACKEND_URL + url, options);
+  const data = await response.json();
+  if (response.status === 401) {
+    window.location.href = '/';
+  }
+  return data;
+};
+
+export const login = async (email, password) => {
+  const response = await fetch(process.env.REACT_APP_BACKEND_URL + '/api/client/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  const data = await response.json();
+  if (response.status === 200) {
+    localStorage.setItem(LOCAL_STORAGE_JWT_KEY, data.token);
+    return data;
+  } else {
+    if (data.error) {
+      alert(data.error);
+    } else {
+      alert('An error occurred while logging in.');
+    }
+    return null;
+  }
+};
+
+export const register = async (email, password) => {
+  const response = await fetch(process.env.REACT_APP_BACKEND_URL + '/api/client', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+  const data = await response.json();
+  if (response.status === 201) {
+
+  } else {
+    if (data.error) {
+      alert(data.error);
+    } else {
+      alert('Email already exists');
+    }
+  }
+};
+
+export const getDecodedToken = () => {
+  const token = localStorage.getItem(LOCAL_STORAGE_JWT_KEY);
+  const body = token.split('.')[1];
+  const decode = JSON.parse(atob(body));
+  return decode
+}
